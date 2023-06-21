d21ff9e9355866dec305c97fdb47de66c99dd420
abd-jalel <benabdallah.jalel@esprit.tn>
2023-06-21 14:33:06 +0100

    integ

--- src/main/java/com/stage/digibackend/services/Userservice.java
+++ src/main/java/com/stage/digibackend/services/Userservice.java
@@ -44,8 +44,8 @@ public class Userservice implements IUserservice {
     }
 
     public void sendVerificationEmail(User user, String siteURL) throws MessagingException, UnsupportedEncodingException {
-        String toAddress = "aladin.hammouda@esprit.tn";
-        String fromAddress = "aladin.hammouda@esprit.tn";
+        String toAddress = user.getEmail();
+        String fromAddress = "alert@dig2s.com";
         String senderName = "Digi-Smart-Solution";
         String subject = "Please verify your registration";
         String content = "Dear [[name]],<br>"
@@ -60,10 +60,25 @@ public class Userservice implements IUserservice {
         helper.setTo(toAddress);
         helper.setSubject(subject);
         content = content.replace("[[name]]", user.getUsername());
-        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
+        String verifyURL =   "http://localhost:1112/users/verify/" + user.getVerificationCode();
         content = content.replace("[[URL]]", verifyURL);
         helper.setText(content, true);
         mailSender.send(message);
+    }
+
+    public boolean verify(String verificationCode) {
+        User user = userRepository.findByVerificationCode(verificationCode);
+
+        if (user == null || user.isEnabled()) {
+            return false;
+        } else {
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+            userRepository.save(user);
+
+            return true;
+        }
+
     }
 
 
@@ -126,7 +141,7 @@ public class Userservice implements IUserservice {
         user.setVerify(randomCode);
         userRepository.save(user);
         String toAddress = email;
-        String fromAddress = "aladin.hammouda@esprit.tn";
+        String fromAddress = "alert@dig2s.com";
         String senderName = "Digi-Smart-Solution";
         String subject = "Your verify code:";
         String content = " <!DOCTYPE html><html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\"><head>\n" +
