78855a47064164bea674906fb7177ca7fdbacea4
Youssrra~ <yousra.abid@esprit.tn>
2023-07-03 10:49:53 +0200

    sensor + device collection

---
+++ src/main/java/com/stage/digibackend/Collections/Sensor.java
@@ -0,0 +1,28 @@
+package com.stage.digibackend.Collections;
+
+import com.stage.digibackend.Enumeration.EUnite;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Document(collection = "sensors")
+@AllArgsConstructor
+@Data
+@Builder
+public class Sensor {
+
+    @Id
+    private String sensorId ;
+    @NotBlank
+    private String sensorName ;
+    @NotBlank
+    private Double rangeMin ;
+    @NotBlank
+    private Double rangeMax ;
+    private String image ;
+    private EUnite unit ;
+
+
+}
