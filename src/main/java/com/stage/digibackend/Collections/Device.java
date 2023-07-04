d279a0f3fc7e2c0bb363974caadfe0910b26ac66
sana9911 <sana.khamassi5678@gmail.com>
2023-07-04 09:58:06 +0100

    send mail with mac@

--- src/main/java/com/stage/digibackend/Collections/Device.java
+++ src/main/java/com/stage/digibackend/Collections/Device.java
@@ -11,14 +11,12 @@ import java.util.List;
 @Data
 @Builder
 public class Device {
-
     @Id
     private String deviceId ;
     @NotBlank
     private String macAdress ;
     @NotBlank
     private String Description ;
-    @DBRef
     private List<String> sensorList ;
     private Boolean active;
     private String idAdmin;
