0d9d0e3950f13a3dab607f938e2955cd457c2392
Youssrra~ <yousra.abid@esprit.tn>
2023-07-11 09:28:39 +0200

    historique find by device / device and sensor

--- src/main/java/com/stage/digibackend/services/IDataSensorService.java
+++ src/main/java/com/stage/digibackend/services/IDataSensorService.java
@@ -3,9 +3,11 @@ package com.stage.digibackend.services;
 import com.stage.digibackend.Collections.DataSensor;
 import com.stage.digibackend.Enumeration.GrowthStatus;
 
+import java.time.LocalDateTime;
+
 public interface IDataSensorService {
 
     DataSensor affecteSensorDevice (String idSensor, String idDevice);
 
-    DataSensor loadDataInSensorDevice (String idSensor, String idDevice, Double data, GrowthStatus growthStatus);
+    DataSensor loadDataInSensorDevice (String idSensor, String idDevice, Double data, GrowthStatus growthStatus, LocalDateTime latestUpdate);
 }
