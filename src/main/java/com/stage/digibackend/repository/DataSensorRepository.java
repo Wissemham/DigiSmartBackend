76a9870fe8195df2a86b621c98d6c892e0d3d9e8
wissem189 <wissem.hammouda@esprit.tn>
2023-07-12 00:03:15 +0100

    datapdf

--- src/main/java/com/stage/digibackend/repository/DataSensorRepository.java
+++ src/main/java/com/stage/digibackend/repository/DataSensorRepository.java
@@ -5,7 +5,10 @@ import com.stage.digibackend.Collections.Device;
 import com.stage.digibackend.Collections.Sensor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface DataSensorRepository extends MongoRepository<DataSensor, String> {
 
     DataSensor findDataSensorByDeviceAndSensor(Device device, Sensor sensor);
+    DataSensor findDataSensorByDeviceAndSensor(Optional<Device> device, Sensor sensor);
 }
