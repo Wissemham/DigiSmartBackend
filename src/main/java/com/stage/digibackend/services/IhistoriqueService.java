e94a86df5e78f9d84e12e05927601c7704de3cc9
sana9911 <sana.khamassi5678@gmail.com>
2023-07-12 16:19:49 +0100

    exportCsv

--- src/main/java/com/stage/digibackend/services/IhistoriqueService.java
+++ src/main/java/com/stage/digibackend/services/IhistoriqueService.java
@@ -3,6 +3,8 @@ package com.stage.digibackend.services;
 import com.stage.digibackend.Collections.Historique;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface IhistoriqueService {
@@ -13,4 +15,5 @@ public interface IhistoriqueService {
     List<Historique> findHistoriqueByDeviceAndSensor(String idDevice, String idSensor) ;
 
     byte[] generateDataSensorHistoriquePdf(String dataSensorId) throws IOException;
+    void exportToCSV(String deviceId, LocalDateTime startDate, LocalDateTime endDate);
 }
